f831f8fc8ae305bfd26ec0fdd0d0351bd1d11af8
Sven Schliesing <sven@schliesing.de>
2024-08-09 14:09:37 +0200

    Only generate RSA key once, fix claims

--- jwt-plugin-server/src/main/java/de/ndr/teamcity/JwtBuildFeature.java
+++ jwt-plugin-server/src/main/java/de/ndr/teamcity/JwtBuildFeature.java
@@ -2,29 +2,69 @@ package de.ndr.teamcity;
 
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.RSAKey;
-import com.nimbusds.jose.jwk.gen.RSAKeyGenerator;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import jetbrains.buildServer.ExtensionHolder;
-import jetbrains.buildServer.serverSide.BuildStartContext;
-import jetbrains.buildServer.serverSide.BuildStartContextProcessor;
-import jetbrains.buildServer.serverSide.SRunningBuild;
+import jetbrains.buildServer.serverSide.*;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.ObjectUtils;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.joda.time.DateTime;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.text.ParseException;
+import java.util.List;
 
-public class JwtBuildFeature implements BuildStartContextProcessor {
+public class JwtBuildFeature extends BuildFeature implements BuildStartContextProcessor {
     private final ExtensionHolder extensionHolder;
 
-    public JwtBuildFeature(@NotNull final ExtensionHolder extensionHolder) {
+    private final SBuildServer buildServer;
+    private final File keyFile;
+
+    public JwtBuildFeature(@NotNull final ExtensionHolder extensionHolder, @NotNull ServerPaths serverPaths, @NotNull SBuildServer buildServer) {
         this.extensionHolder = extensionHolder;
+        this.buildServer = buildServer;
+
+        File directory = new File(serverPaths.getPluginDataDirectory() + File.separator + "JwtBuildFeature");
+        directory.mkdirs();
+        this.keyFile = new File(directory + File.separator + "key.json");
     }
 
     public void register() {
         extensionHolder.registerExtension(BuildStartContextProcessor.class, this.getClass().getName(), this);
+    }
+
+    @NotNull
+    @Override
+    public String getType() {
+        return "";
+    }
+
+    @NotNull
+    @Override
+    public String getDisplayName() {
+        return "JWT";
+    }
+
+    @Nullable
+    @Override
+    public String getEditParametersUrl() {
+        return "";
+    }
+
+    @Override
+    public boolean isRequiresAgent() {
+        return false;
     }
 
     @Override
@@ -43,18 +83,20 @@ public class JwtBuildFeature implements BuildStartContextProcessor {
                     .build();
 
             DateTime now = new DateTime();
+            // Must start with https:// in order to be accepted for OIDC
+            String buildServerRootUrl = buildServer.getRootUrl().replaceFirst("http://", "https://");
             JWTClaimsSet jwtClaimsSet = new JWTClaimsSet.Builder()
-                    .subject(build.getBuildTypeExternalId() + "_" + build.getBuildNumber())
-                    .audience("example audience")
-                    .issuer("https://teamcity.example.com")
+                    .subject(build.getBuildTypeExternalId())
+                    .audience(List.of(buildServerRootUrl))
+                    .issuer(buildServerRootUrl)
                     .issueTime(now.toDate()) // iat
                     .notBeforeTime(now.toDate()) // nbf
                     .expirationTime(now.plusHours(1).toDate()) // exp
                     .claim("branch", ObjectUtils.defaultIfNull(build.getBranch(), ""))
-                    .claim("buildTypeExternalId", build.getBuildTypeExternalId())
-                    .claim("triggeredById", build.getTriggeredBy().getUser().getId())
-                    .claim("triggeredBy", build.getTriggeredBy().getAsString())
-                    .claim("buildNumber", build.getBuildNumber())
+                    .claim("build_type_external_id", build.getBuildTypeExternalId())
+                    .claim("triggered_by_id", build.getTriggeredBy().getUser().getId())
+                    .claim("triggered_by", build.getTriggeredBy().getAsString())
+                    .claim("build_number", build.getBuildNumber())
                     .build();
 
             SignedJWT signedJWT = new SignedJWT(jwsHeader, jwtClaimsSet);
@@ -62,19 +104,31 @@ public class JwtBuildFeature implements BuildStartContextProcessor {
             signedJWT.sign(signer);
             String jwtToken = signedJWT.serialize();
 
-            buildStartContext.addSharedParameter("env.JWT", rsaKey.toPublicJWK().toString());
-            buildStartContext.addSharedParameter("env.JWT_TOKEN", jwtToken);
-        } catch (JOSEException e) {
+            buildStartContext.addSharedParameter("env.JWK", rsaKey.toPublicJWK().toString());
+            buildStartContext.addSharedParameter("env.JWT", jwtToken);
+        } catch (JOSEException | UnrecoverableKeyException | CertificateException | KeyStoreException | IOException | NoSuchAlgorithmException | ParseException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static RSAKey getRsaKey() throws JOSEException {
-        return new RSAKeyGenerator(2048)
-                .keyID("teamcity")
-                .keyUse(KeyUse.SIGNATURE)
-                .algorithm(JWSAlgorithm.RS256)
-                .generate();
+    private RSAKey getRsaKey() throws JOSEException, CertificateException, KeyStoreException, IOException, NoSuchAlgorithmException, UnrecoverableKeyException, ParseException {
+        JWK key;
+        if (this.keyFile.exists()) {
+            key = JWK.parse(FileUtils.readFileToString(this.keyFile, Charset.defaultCharset()));
+        } else {
+            KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
+            gen.initialize(2048);
+            KeyPair keyPair = gen.generateKeyPair();
+
+            key = new RSAKey.Builder((RSAPublicKey) keyPair.getPublic())
+                    .privateKey((RSAPrivateKey) keyPair.getPrivate())
+                    .keyUse(KeyUse.SIGNATURE)
+                    .keyID("teamcity")
+                    .algorithm(JWSAlgorithm.RS256)
+                    .build();
+            FileUtils.writeStringToFile(this.keyFile, key.toString(), Charset.defaultCharset());
+        }
+        return key.toRSAKey();
     }
 
 }
