9f1d4bc7f32e149009a2b564cc0f5511a120fa60
Sven Schliesing <sven@schliesing.de>
2024-08-12 22:10:05 +0200

    add tests

---
+++ jwt-plugin-server/src/main/test/de/ndr/teamcity/JwtBuildFeatureTest.java
@@ -0,0 +1,55 @@
+package de.ndr.teamcity;
+
+import jetbrains.buildServer.serverSide.ServerPaths;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
+
+@ExtendWith(MockitoExtension.class)
+public class JwtBuildFeatureTest {
+
+    @Mock
+    private ServerPaths serverPaths;
+
+    @TempDir
+    private File tempDir;
+
+    @Test
+    public void testGetRsaKeyCreatesFile() throws NoSuchAlgorithmException, IOException, ParseException {
+        File pluginDirectory = new File(tempDir + File.separator + new File("foobar"));
+        pluginDirectory.mkdirs();
+        when(serverPaths.getPluginDataDirectory()).thenReturn(pluginDirectory);
+        File keyFile = new File(pluginDirectory + File.separator + "JwtBuildFeature" + File.separator + "key.json");
+        JwtBuildFeature jwtBuildFeature = new JwtBuildFeature(serverPaths);
+        assertTrue(keyFile.exists());
+        assertEquals(jwtBuildFeature.getRsaKey().toString(), FileUtils.readFileToString(keyFile, Charset.defaultCharset()));
+    }
+
+    @Test
+    public void testGetRsaKeyReusesFile() throws NoSuchAlgorithmException, IOException, ParseException {
+
+        File pluginDirectory = new File(tempDir + File.separator + new File("foobar"));
+        pluginDirectory.mkdirs();
+        when(serverPaths.getPluginDataDirectory()).thenReturn(pluginDirectory);
+        File keyFile = new File(pluginDirectory + File.separator + "JwtBuildFeature" + File.separator + "key.json");
+
+        new JwtBuildFeature(serverPaths);
+        String keyFileContents = FileUtils.readFileToString(keyFile, Charset.defaultCharset());
+
+        new JwtBuildFeature(serverPaths);
+        String keyFileContents2 = FileUtils.readFileToString(keyFile, Charset.defaultCharset());
+        assertEquals(keyFileContents, keyFileContents2);
+    }
+
+}
---
+++ jwt-plugin-server/src/main/test/de/ndr/teamcity/JwtBuildStartContextTest.java
@@ -0,0 +1,83 @@
+package de.ndr.teamcity;
+
+import jetbrains.buildServer.ExtensionHolder;
+import jetbrains.buildServer.serverSide.*;
+import jetbrains.buildServer.users.SUser;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class JwtBuildStartContextTest {
+
+    @Mock
+    ExtensionHolder extensionHolder;
+
+    @Mock
+    SBuildServer buildServer;
+
+    @Mock
+    SRunningBuild runningBuild;
+
+    @Mock
+    BuildStartContext buildStartContext;
+
+    @Mock
+    SBuildFeatureDescriptor jwtBuildFeatureBuildFeatureDescriptor;
+
+    @Mock
+    private ServerPaths serverPaths;
+
+    @TempDir
+    private File tempDir;
+
+    @Test
+    public void testRegister() {
+        JwtBuildStartContext jwtBuildStartContext = new JwtBuildStartContext(extensionHolder, buildServer);
+        jwtBuildStartContext.register();
+
+        verify(extensionHolder, times(1)).registerExtension(any(), any(), any());
+    }
+
+    @Test
+    public void doNotUpdateParametersWhenBuildFeatureDisabled() {
+        JwtBuildStartContext jwtBuildStartContext = new JwtBuildStartContext(extensionHolder, buildServer);
+        when(buildStartContext.getBuild()).thenReturn(runningBuild);
+        when(runningBuild.getBuildFeaturesOfType("JWT-Plugin")).thenReturn(Collections.emptyList());
+        jwtBuildStartContext.updateParameters(buildStartContext);
+        verify(buildStartContext, never()).addSharedParameter(any(), any());
+    }
+
+    @Test
+    public void updateParametersWhenBuildFeatureEnabled() throws NoSuchAlgorithmException, IOException, ParseException {
+        when(serverPaths.getPluginDataDirectory()).thenReturn(tempDir);
+        JwtBuildFeature jwtBuildFeature = new JwtBuildFeature(serverPaths);
+
+        when(buildServer.getRootUrl()).thenReturn("http://localhost:8111");
+        JwtBuildStartContext jwtBuildStartContext = new JwtBuildStartContext(extensionHolder, buildServer);
+
+        when(buildStartContext.getBuild()).thenReturn(runningBuild);
+        when(runningBuild.getBuildFeaturesOfType("JWT-Plugin")).thenReturn(List.of(jwtBuildFeatureBuildFeatureDescriptor));
+        when(jwtBuildFeatureBuildFeatureDescriptor.getBuildFeature()).thenReturn(jwtBuildFeature);
+
+        TriggeredBy triggeredByMock = mock(TriggeredBy.class);
+        SUser userMock = mock(SUser.class);
+        when(runningBuild.getTriggeredBy()).thenReturn(triggeredByMock);
+        when(triggeredByMock.getUser()).thenReturn(userMock);
+
+        jwtBuildStartContext.updateParameters(buildStartContext);
+        verify(buildStartContext, times(1)).addSharedParameter(eq("env.JWT"), any());
+    }
+
+}
